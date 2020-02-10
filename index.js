8abe0c0ce1bb89d5174ef92a87717e7a51106845
Alexandra Ignatova <alex.ignatova@gmail.com>
2020-02-10 14:38:50 +0100

    feat: add about-mobile

--- index.js
+++ index.js
@@ -1,3 +1,14 @@
+
+
+const header = document.querySelector(".page-header");
+const menuButton = document.querySelector(".page-header__toggle");
+
+
+menuButton.addEventListener("click", event =>{
+    header.classList.toggle("page-header--open");
+});
+
+
 class Carousel {
   constructor(selector) {
     this.position = 0;
@@ -27,16 +38,9 @@ class Carousel {
   }
 }
 
-const header = document.querySelector(".page-header");
-const menuButton = document.querySelector(".page-header__toggle");
-
 const btn_prev = document.querySelector(".slider__button--prev");
 const btn_next = document.querySelector(".slider__button--next");
 const carousel = new Carousel(".about__slider__wrapper");
-
-menuButton.addEventListener("click", event =>{
-    header.classList.toggle("page-header--open");
-});
 
 btn_prev.addEventListener("click", () => {
   carousel.decrementPosition();
